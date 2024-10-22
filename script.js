0f43e54a46ddea15ed28aaf60e9f1a3bc7eac695
Ritank Jaikar <ritank.jaikar@gmail.com>
2024-10-22 17:38:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 //your JS code here. If required.
 const container = document.querySelector(".container");
 
-for(let i=1; i<800; i++) {
+for(let i=1; i<10000; i++) {
 	let square = document.createElement("div");
 	square.classList.add("square");
 	container.append(square);
 	square.addEventListener("mouseenter", () => {
-	    let r = Math.floor(Math.random() * (255 - 0 + 1)) + 0;
-	    let g = Math.floor(Math.random() * (255 - 0 + 1)) + 0;
-	    let b = Math.floor(Math.random() * (255 - 0 + 1)) + 0;
-	    square.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+		let r = Math.floor(Math.random() * (255 - 0 + 1)) + 0;
+		let g = Math.floor(Math.random() * (255 - 0 + 1)) + 0;
+		let b = Math.floor(Math.random() * (255 - 0 + 1)) + 0;
+		square.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
 	});
 	square.addEventListener("mouseleave", () => {
-	    square.style.backgroundColor = 'rgb(29, 29, 29)';
+		square.style.backgroundColor = 'rgb(29, 29, 29)';
 	})
 }
