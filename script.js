5f1dcbce0e5ddb00e1ef2f748574b7e021b66e4e
Ritank Jaikar <ritank.jaikar@gmail.com>
2024-10-22 17:35:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,12 +6,12 @@ for(let i=1; i<800; i++) {
 	square.classList.add("square");
 	container.append(square);
 	square.addEventListener("mouseenter", () => {
-		let r = Math.floor(Math.random() * (255 - 0 + 1)) + 0;
-		let g = Math.floor(Math.random() * (255 - 0 + 1)) + 0;
-		let b = Math.floor(Math.random() * (255 - 0 + 1)) + 0;
-		square.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+	    let r = Math.floor(Math.random() * (255 - 0 + 1)) + 0;
+	    let g = Math.floor(Math.random() * (255 - 0 + 1)) + 0;
+	    let b = Math.floor(Math.random() * (255 - 0 + 1)) + 0;
+	    square.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
 	});
 	square.addEventListener("mouseleave", () => {
-		square.style.backgroundColor = 'rgb(29, 29, 29)';
+	    square.style.backgroundColor = 'rgb(29, 29, 29)';
 	})
 }
