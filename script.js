fbdb3bbddbf74eb8b82cbc23db0fc71dd0345aa4
Ritank Jaikar <ritank.jaikar@gmail.com>
2024-10-22 18:16:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,11 +2,10 @@
 document.addEventListener(DOMContentLoaded, functon(){
 	const squares = document.querySelectorAll('.square');
 	squares.forEach(square => {
-		square.addEventListener(mouseover, function()
-								{
-									square.style.backgroundColor = '#3498db';
-								});
-		square.addEventListener(mouseout, function(){
+		square.addEventListener(mouseover, function() {
+			square.style.backgroundColor = '#3498db';
+		});
+		square.addEventListener(mouseout, function() {
 			setTimeout(()=>{
 				squares.style.backgroundColor = '#eee';
 			},1000);
